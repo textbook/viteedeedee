3430d16ccb4da6f6ec7930a8ab43bfcf319fd4fc
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2024-01-13 16:24:27 +0000

    Take screenshots on E2E failure

--- playwright.config.js
+++ playwright.config.js
@@ -26,6 +26,9 @@ export default defineConfig({
 		/* Base URL to use in actions like `await page.goto('/')`. */
 		baseURL: "http://localhost:4173",
 
+		/* Take a screenshot if a test fails */
+		screenshot: "only-on-failure",
+
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		trace: "on-first-retry",
 	},
