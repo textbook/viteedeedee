31a053406a39e89cb515c03fce0ce17059ba0685
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2024-01-13 15:17:29 +0000

    Add CYF linting rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,27 @@
+/** @type {import("eslint").Linter.BaseConfig} */
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+	root: true,
+	env: { browser: true, es2020: true },
+	extends: [
+		"@codeyourfuture/standard",
+		"plugin:react/recommended",
+		"plugin:react/jsx-runtime",
+		"plugin:react-hooks/recommended",
+	],
+	ignorePatterns: ["dist"],
+	overrides: [
+		{
+			files: [".eslintrc.cjs", "vite.config.js"],
+			env: { browser: false, node: true },
+		},
+	],
+	parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+	plugins: ["react-refresh"],
+	rules: {
+		"react-refresh/only-export-components": [
+			"warn",
+			{ allowConstantExport: true },
+		],
+	},
+	settings: { react: { version: "18.2" } },
+};
